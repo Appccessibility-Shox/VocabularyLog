48dce19ed5fcd29647eed1084993be0e324ce5fc
Reiny Botros <reinybo@gmail.com>
2021-03-01 21:18:19 -0500

    Initial Commit

---
+++ VocabularyLog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VocabularyLog
+//
+//  Created by Patrick Botros on 3/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VocabularyLog/VocabularyLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyLogApp.swift
+//  VocabularyLog
+//
+//  Created by Patrick Botros on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
