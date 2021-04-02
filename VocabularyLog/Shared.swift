3a28f6317f961ebad72b8af711dda25a83770aeb
Patrick Botros <patrick.botros@duke.edu>
2021-04-01 21:38:28 -0400

    Added functionality to import a log and export your current log (to back up).

--- VocabularyLog/Shared.swift
+++ VocabularyLog/Shared.swift
@@ -1,11 +1,19 @@
 import Foundation
 
-struct Term: Codable {
+struct Term: Codable, Hashable {
     var word: String
     var exampleSentence: String
     var url: String?
     var preferredDef: String?
     var id = UUID()
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+    static func ==(left:Term, right:Term) -> Bool {
+        return left.id == right.id
+    }
 }
 
 let appGroupID = "L27L4K8SQU.VocabularyLog"
